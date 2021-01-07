ef08d25f9045edbef02201c6a8c4f4aa7d9821b4
Sean Kuehl <teddscottfelt@gmail.com>
2021-01-07 13:36:38 -0500

    Finished up the single player game, next I'll try to do multiplayer

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 
 
@@ -8,6 +9,30 @@ int main(void) {
 	sf::CircleShape ball(10.f);
 	ball.setFillColor(sf::Color::White);
 	ball.setPosition(500, 400);
+	
+	float ballXSpeed = 0.05;
+	float ballYSpeed = 0.05;
+	int playerOneScore = 0;	//this is the player on the left
+	int playerTwoScore = 0;	//this is the player on the right
+
+	std::string str;
+
+	sf::Text playerOneText;
+	sf::Text playerTwoText;
+	sf::Font font;
+	if (!font.loadFromFile("sans.ttf")) {
+		std::cout << "failed\n";
+		exit(EXIT_FAILURE);
+	}
+	playerOneText.setFont(font);
+	playerOneText.setCharacterSize(24);
+	playerOneText.setFillColor(sf::Color::White);
+	playerOneText.setPosition(sf::Vector2f(400, 10));
+
+	playerTwoText.setFont(font);
+	playerTwoText.setCharacterSize(24);
+	playerTwoText.setFillColor(sf::Color::White);
+	playerTwoText.setPosition(sf::Vector2f(600, 10));
 
 	sf::RectangleShape rightPaddle(sf::Vector2f(950, 10));	//this might be size, it seems useless
 	rightPaddle.setSize(sf::Vector2f(20, 100));
@@ -52,11 +77,61 @@ int main(void) {
 		if ((leftPaddle.getGlobalBounds().top + leftPaddle.getGlobalBounds().height) > 790) {
 			leftPaddle.setPosition(sf::Vector2f(50, 680));
 		}
+		if (rightPaddle.getGlobalBounds().top < 0) {
+			rightPaddle.setPosition(sf::Vector2f(950, 10));
+		}
+		if ((rightPaddle.getGlobalBounds().top + rightPaddle.getGlobalBounds().height) > 790) {
+			rightPaddle.setPosition(sf::Vector2f(950, 680));
+		}
+
+		ball.move(sf::Vector2f(ballXSpeed, ballYSpeed));
+
+		//when the ball goes out of screen in the different directions
+		if (ball.getGlobalBounds().top < 0) {
+			ballYSpeed = ballYSpeed * -1;
+		}
+		if ((ball.getGlobalBounds().top + ball.getGlobalBounds().height) > 790) {
+			ballYSpeed = ballYSpeed * -1;
+		}
+		if (ball.getGlobalBounds().left < 0) {
+			//it went out the left side
+			playerTwoScore += 1;
+			
+			ball.setPosition(500, 400);
+			ballXSpeed = ballXSpeed * -1;
+			ballYSpeed = ballYSpeed * -1;
+		}
+		if ((ball.getGlobalBounds().left + ball.getGlobalBounds().width) > 1000) {
+			playerOneScore += 1;
+			ball.setPosition(500, 400);
+			ballXSpeed = ballXSpeed * -1;
+			ballYSpeed = ballYSpeed * -1;
+		}
+
+		//when ball collides
+		if (ball.getGlobalBounds().intersects(leftPaddle.getGlobalBounds())) {
+			
+			ballXSpeed = ballXSpeed * -1;
+		}
+		if (ball.getGlobalBounds().intersects(rightPaddle.getGlobalBounds())) {
+			
+			ballXSpeed = ballXSpeed * -1;
+		}
+
+		str = std::to_string(playerOneScore);
+		playerOneText.setString(str);
+		str = std::to_string(playerTwoScore);
+		playerTwoText.setString(str);
+
+		
+		
 
 		window.clear();
 		window.draw(leftPaddle);
 		window.draw(rightPaddle);
 		window.draw(ball);
+		window.draw(playerOneText);
+		window.draw(playerTwoText);
 
 		window.display();
 		
