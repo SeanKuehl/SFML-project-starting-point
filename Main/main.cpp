59584fc80daf6950b48e1d3f9a217dd6a187ee92
Sean Kuehl <teddscottfelt@gmail.com>
2021-01-07 11:17:51 -0500

    made both pong paddles and a ball. Made sure left paddle can't go out of window.

--- Main/main.cpp
+++ Main/main.cpp
@@ -2,10 +2,28 @@
 #include <SFML/Graphics.hpp>
 
 
+
 int main(void) {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
+	sf::CircleShape ball(10.f);
+	ball.setFillColor(sf::Color::White);
+	ball.setPosition(500, 400);
+
+	sf::RectangleShape rightPaddle(sf::Vector2f(950, 10));	//this might be size, it seems useless
+	rightPaddle.setSize(sf::Vector2f(20, 100));
+	rightPaddle.setFillColor(sf::Color::White);
+	rightPaddle.setPosition(sf::Vector2f(950, 10));
+
+	sf::RectangleShape leftPaddle(sf::Vector2f(50,10));
+	leftPaddle.setSize(sf::Vector2f(20, 100));
+	leftPaddle.setFillColor(sf::Color::White);
+	leftPaddle.setPosition(sf::Vector2f(50, 10));
+	//use move() which will shift it so much from it's current position
+	//use primitives for the pong paddles
+	//leftPaddle.getGlobalBounds();	//this gets a rect
+	//there is a collision thing with rects
+	//intersects (const Rect< T > &rectangle, Rect< T > &intersection) 
+	//returns bool on whether or not the two rects are colliding
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -13,10 +31,33 @@ int main(void) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+				leftPaddle.move(sf::Vector2f(0, -10));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+				leftPaddle.move(sf::Vector2f(0, 10));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+				rightPaddle.move(sf::Vector2f(0,10));
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+				rightPaddle.move(sf::Vector2f(0, -10));
+			}
+		}
+
+		//check for variouse things
+		if (leftPaddle.getGlobalBounds().top < 0) {
+			leftPaddle.setPosition(sf::Vector2f(50, 10));
+		}
+		if ((leftPaddle.getGlobalBounds().top + leftPaddle.getGlobalBounds().height) > 790) {
+			leftPaddle.setPosition(sf::Vector2f(50, 680));
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(leftPaddle);
+		window.draw(rightPaddle);
+		window.draw(ball);
+
 		window.display();
 		
 	}
